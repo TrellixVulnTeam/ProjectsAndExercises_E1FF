1f9a1da388695493247bdb0ec193cb3540621092
TheFlorin97 <constantin.florin97@yahoo.com>
2021-02-09 16:22:27 +0200

    #1
    Start from the basics

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,21 @@
+//Coding Challenge #1
+
+// var markWeight = 79;
+// var markHeight = 1.69;
+// var johnWeight = 92;
+// var johnHeight = 1.95;
+
+// var markBmi = markWeight / markHeight ** 2;
+// console.log("Mark BMI:" + " " + markBmi);
+
+// var jhonBmi = johnWeight / johnHeight ** 2;
+// console.log("Jhon BMI:" + " " + jhonBmi);
+
+// var markHigherBMI = markBmi > jhonBmi;
+
+// if (markHigherBMI)
+//     console.log("Mark has a higher BMI than Jhon.")
+
+
+// Coding Challenge #2
+
