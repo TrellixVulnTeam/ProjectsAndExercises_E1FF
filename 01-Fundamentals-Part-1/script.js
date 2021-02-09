3fb625333c7f652e2626a534a429f8ff3b7f6f1f
TheFlorin97 <constantin.florin97@yahoo.com>
2021-02-09 17:32:02 +0200

    #3

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,5 @@
-//Coding Challenge #1
+// Coding Challenge #1
+// Coding Challenge #2
 
 // var markWeight = 79;
 // var markHeight = 1.69;
@@ -13,9 +14,45 @@
 
 // var markHigherBMI = markBmi > jhonBmi;
 
-// if (markHigherBMI)
-//     console.log("Mark has a higher BMI than Jhon.")
+// if (markHigherBMI) {
+//     console.log('Mark ' + markBmi + ' has a higher BMI than Jhon' + jhonBmi);
+// }
+// else {
+//     console.log('John ' + jhonBmi + ' has a higher BMI than Mark ' + markBmi)
+// }
 
+// Coding Challenge #3
 
-// Coding Challenge #2
+// var d1 = 96;
+// var d2 = 108;
+// var d3 = 89;
+
+// var k1 = 88;
+// var k2 = 91;
+// var k3 = 110;
+
+// if (d1 > k1 && d2 > k2 || d3 > k3) {
+//     console.log('Dolpins are the winners')
+// } else if (d1 > k1 || d2 > k2 && d3 > k3) {
+//     consoele.log('Dolpins are the winners')
+// }
+// else if (d1 === k1 && d2 === k2 || d3 === k3) {
+//     console.log('Draw')
+// }
+// else if (d1 === k1 || d2 === k2 && d3 === k3) {
+//     console.log('Draw')
+// } else
+//     consoele.log('Koalas are the winners')
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
 
