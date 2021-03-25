ada67fac57a1c1aa749741f72adc5dfca075a9b2
TheFlorin97 <constantin.florin97@yahoo.com>
2021-03-25 18:47:18 +0200

    Budget app
    This is an app that I made following a tutorial .In this exercise a learned better ways plan and construct an app ,function constructors and modules.

---
+++ Budget App/app.js
@@ -0,0 +1,97 @@
+var budgetController = (function () {
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+})();
+
+var UIController = (function () {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var setupEventListener = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+    }
+
+
+
+    var ctrlAddItem = function () {
+
+        // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+    }
+
+    return {
+        init: function () {
+            console.log('Application has started.');
+            setupEventListener();
+        }
+    }
+
+})(budgetController, UIController);
+
+
+controller.init();
